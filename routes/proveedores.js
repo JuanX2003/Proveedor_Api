35d0951f1ec1b41fb2b44437639ea6725dbc7840
JuanX2003 <166203301+JuanX2003@users.noreply.github.com>
2024-04-08 16:52:26 -0500

    proveedores.js

--- routes/proveedores.js
+++ routes/proveedores.js
@@ -1,17 +1,20 @@
 const { Router } = require('express');
 const router = Router();
-const { proveedorGet, proveedorPost, proveedorPut, proveedorDelete } = require('../controllers/proveedor');
+const { proveedorGet, proveedorPost, proveedorPut, proveedorDelete, proveedorGetById } = require('../controllers/proveedor');
 
 // Ruta para obtener todos los proveedores (GET '/')
 router.get('/', proveedorGet);
+
+// Ruta para obtener un proveedor por su ID (GET '/:id_proveedor')
+router.get('/:id_proveedor', proveedorGetById);
 
 // Ruta para crear un nuevo proveedor (POST '/')
 router.post('/', proveedorPost);
 
 // Ruta para actualizar un proveedor existente (PUT '/:idProveedor')
-router.put('/:id_proveedor', proveedorPut); // Cambiar _id a id_proveedor
+router.put('/:id_proveedor', proveedorPut);
 
 // Ruta para eliminar un proveedor existente (DELETE '/:idProveedor')
-router.delete('/:id_proveedor', proveedorDelete); // Cambiar _id a id_proveedor
+router.delete('/:id_proveedor', proveedorDelete);
 
 module.exports = router;
